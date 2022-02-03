0b39171bc516ff1c2a504f2590ff82e2485e7725
david_bille <David.Bille@precisionvh.com>
2022-02-02 22:07:39 -0600

    set up functionality for adding and storing candidate models

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,31 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-  // Constructor to 1: Store candidate, 2: Read candidate
-  string public candidate;
-  constructor() public {
-    candidate = 'Frankie Offenheim';
+  // Model a candidate
+  struct Candidate { // struct is like a class
+    uint id;
+    string name;
+    uint voteCount;
   }
+  
+  // Store candidates
+  // Fetch candidates
+  // creating a map(associative array) - each key is a uint with value being a candidate - publicly accessible - name of the map is 'candidates'
+  // when adding candidate to map, we are changing the state of our smart contract, and writing to the data layer of the blockchain
+  mapping(uint => Candidate) public candidates;
+  // Store number of candidates to allow us to fetch each from the mapping
+  uint public candidatesCount;
+
+  constructor() public {
+    addCandidate('Frankie Offenheimer');
+    addCandidate('Tabitha Schmidt');
+  }
+
+  // _name in parameter so as not to clash with name in Candidate struct. it's a local var, not state var
+  // private - can only be called by this Election contract
+  function addCandidate(string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
 }
